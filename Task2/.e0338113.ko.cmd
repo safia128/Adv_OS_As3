ce81b42a4d522526b019e4bdbab47f374276963f
Safia <safia128@gmail.com>
2019-03-24 22:38:58 +0800

    Implementing R/W

---
+++ Task2/.e0338113.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/safiafathimamohamediqbal/Desktop/Adv_OS_As3/Task2/e0338113.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/safiafathimamohamediqbal/Desktop/Adv_OS_As3/Task2/e0338113.ko /home/safiafathimamohamediqbal/Desktop/Adv_OS_As3/Task2/e0338113.o /home/safiafathimamohamediqbal/Desktop/Adv_OS_As3/Task2/e0338113.mod.o ;  true
---
+++ Task2/.tmp_versions/e0338113.mod
@@ -0,0 +1,3 @@
+/home/safiafathimamohamediqbal/Desktop/Adv_OS_As3/Task2/e0338113.ko
+/home/safiafathimamohamediqbal/Desktop/Adv_OS_As3/Task2/e0338113.o
+
